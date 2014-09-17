2be4b3eddb4b82a7e555c395a789d2bb3aebf928
Raukobug <rauko1@gmail.com>
2014-09-17 13:16:30 +0200

    Starten af koden.

---
+++ ProducerConsumer/BoundedBuffer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class BoundedBuffer
+    {
+
+        public Queue<string> q = new Queue<string>(); 
+
+        public Boolean IsFull()
+        {
+            return true;
+        }
+
+        public void Put()
+        {
+            
+
+        }
+
+        public int Take()
+        {
+            
+
+        }
+
+
+    }
+}
