0d20fc5a9de07077df93139f4c2a4562f0f3da13
Raukobug <rauko1@gmail.com>
2014-09-17 13:28:24 +0200

    BoundedBuffer er blevet 80% færdig.

--- ProducerConsumer/BoundedBuffer.cs
+++ ProducerConsumer/BoundedBuffer.cs
@@ -1,32 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProducerConsumer
 {
     class BoundedBuffer
     {
 
-        public Queue<string> q = new Queue<string>(); 
+        public Queue<int> MyQueue = new Queue<int>();
+        public int I = 0;
+        public BoundedBuffer(int i)
+        {
+            I = i;
+        }
 
         public Boolean IsFull()
         {
+            if (I == MyQueue.Count)
+            {
+                return false;
+            }
             return true;
         }
 
-        public void Put()
+        public void Put(int i)
         {
-            
-
+            if (IsFull()) 
+            {
+                var rnd = new Random();
+                int number = rnd.Next(1, 1000);
+                MyQueue.Enqueue(number);
+            }
         }
 
         public int Take()
         {
-            
-
+            return MyQueue.Dequeue();
         }
 
 
